1d9e0ace3a2a40803af16e0e1ed00f9fd497a1a3
vincesaykao <vtsaykao@hotmail.com>
2022-03-04 16:29:05 -0600

    added url and description input, dispatched them from form.js

--- src/components/MovieForm/MovieForm.js
+++ src/components/MovieForm/MovieForm.js
@@ -6,16 +6,24 @@ export default function MovieForm() {
 
     // local state that will be an empty string and be updated with new value upon user input
     const [movieInput, setMovieInput] = useState('');
+    let [ urlInput, setUrlInput ] = useState('');
+    let [descriptionInput, setDescriptionInput ] = useState('');
 
     const dispatch = useDispatch();
 
     // this function will dispatch to root-saga when submit is clicked
     function handleSubmit() {
         console.log('clicked');
-        console.log(movieInput);
+        console.log(movieInput, urlInput, descriptionInput);
 
         dispatch({ type: 'FETCH_MOVIES', payload: { movieInput: movieInput } });
+        dispatch({ type: 'FETCH_MOVIES', payload: { urlInput, urlInput } });
+        dispatch({ type: 'FETCH_MOVIES', payload: { descriptionInput, descriptionInput } });
+
+        // clears input value after submit is pressed
         setMovieInput('');
+        setUrlInput('');
+        setDescriptionInput('');
 
     }
 
@@ -26,6 +34,17 @@ export default function MovieForm() {
     onChange={evt => setMovieInput(evt.target.value)}
     
     />
+
+    <input 
+    value={urlInput}
+    onChange={evt => setUrlInput(evt.target.value)}
+    />
+
+    <textarea 
+    value={descriptionInput}
+    onChange={evt => setDescriptionInput(evt.target.value)}
+    />
+    
     <button
     onClick={handleSubmit}
     >
