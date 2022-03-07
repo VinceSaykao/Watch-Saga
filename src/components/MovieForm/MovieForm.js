afa6eb8b05864477028f7848f1df185068ea1d17
vincesaykao <vtsaykao@hotmail.com>
2022-03-06 18:23:44 -0600

    added material ui inputs

--- src/components/MovieForm/MovieForm.js
+++ src/components/MovieForm/MovieForm.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
 import './MovieForm.css';
+import TextField from '@mui/material/TextField';
 
 export default function MovieForm() {
 
-    let history=useHistory();
+    let history = useHistory();
 
 
-    
+
     // local state that will be an empty string and be updated with new value upon user input
     const [movieInput, setMovieInput] = useState('');
-    let [ urlInput, setUrlInput ] = useState('');
-    let [descriptionInput, setDescriptionInput ] = useState('');
+    let [urlInput, setUrlInput] = useState('');
+    let [descriptionInput, setDescriptionInput] = useState('');
 
     const dispatch = useDispatch();
 
@@ -43,45 +44,55 @@ export default function MovieForm() {
 
     return (
         <>
-        <div id='form-back'>
-    <div id='form-div'>
-    <input 
-    id='movie-input'
-    value={movieInput}
-    onChange={evt => setMovieInput(evt.target.value)}
-    
-    />
-    
-    <input 
-    value={urlInput}
-    onChange={evt => setUrlInput(evt.target.value)}
-    />
-    
-    <textarea 
-    value={descriptionInput}
-    onChange={evt => setDescriptionInput(evt.target.value)}
-    />
-    </div>
+            <div id='form-back'>
+                <div id='form-div'>
 
-    </div>
-<ul>
-    <li>
-        <h3
-        id='save'
-        data-text="save"
-        onClick={handleSubmit}
-        >&nbsp;save</h3>
-        </li>
-        <li>
-        <h3
-        id='cancel'
-        onClick={handleCancel}
-        data-text="back"
-        >&nbsp;back</h3>
+                    <TextField
+                        id="outlined-basic"
+                        label="Title"
+                        variant="outlined"
+                        className='movie-input'
+                        value={movieInput}
+                        onChange={evt => setMovieInput(evt.target.value)}
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="URL"
+                        variant="outlined"
+                        value={urlInput}
+                        onChange={evt => setUrlInput(evt.target.value)}
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Description"
+                        variant="outlined"
+                        value={descriptionInput}
+                        onChange={evt => setDescriptionInput(evt.target.value)}
+                    />
 
-        </li>
-    </ul>
-    </>
+
+
+                </div>
+
+            </div>
+            <ul>
+                <li>
+                    <h3
+                        id='save'
+                        data-text="save"
+                        onClick={handleSubmit}
+                    >&nbsp;save</h3>
+                </li>
+                <li>
+                    <h3
+                        id='cancel'
+                        onClick={handleCancel}
+                        data-text="back"
+                    >&nbsp;back</h3>
+
+                </li>
+            </ul>
+        </>
     )
 
 }; // end of MovieForm
